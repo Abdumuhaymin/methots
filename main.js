2e9fd0d9519d836a0784b31441120050e673c207
uzbfamily57@gmail.com <abdurahmonov06a@gmail.com>
2023-12-21 21:38:36 +0500

    hammasi tayyor

---
+++ main.js
@@ -0,0 +1,108 @@
+
+// 1-masala
+// let str = "salom";
+// let unli = [ 'a', 'i', 'o', 'u', 'oʻ', 'e'];
+// let newStr = str.split("");
+// const y=(arr)=>{
+  // for (let i = 0; i < arr.length; i++) {
+    // for (let j = 0; j < unli.length; j++) {
+      // if (arr[i] == unli[j]) {
+        // arr.splice(i,1)
+      // }else{
+      // }
+    // }
+  // }
+  // return newStr.join('')
+// }
+// let result = y(newStr)
+// console.log(result);
+// 
+// 
+
+// 2-masala
+// let num1 = 9333; 
+// let num2 = 1; 
+// let newA = String(num1).split("");
+// let count = []
+// const y=(arr) =>{
+  // for (let i = 0; i < arr.length; i++) {
+    // count.push(arr[i]-num2);  
+  // }
+  // return Number(count.join("")); 
+// }
+// let result = y(newA);
+// console.log(result);
+// 
+
+// 3-masala
+// let numstr = "-1 -2 3 -4"
+// let numarr = numstr.split("")
+// const y = (arr) =>{
+  // for (let i = 0; i < arr.length; i++) {
+    // if (arr[i] == '-'){
+      // arr.splice(i,1)
+    // }
+  // }
+  // return numarr.join('');
+// }
+// let result = y(numarr);
+// console.log(result);
+// 
+
+
+// 4-masala
+// let str = "salom";
+// let nawstr =str.split("")
+// let count = [];
+// let a = 0;
+// const y=(arr) =>{
+  // for (let i = 0; i < arr.length; i++) {
+    // a+=1
+  // }
+  // if (a%2==0){
+    // count.push(arr[(a/2)-1],arr[a/2]);
+  // }else{
+    // count.push(arr[(a-1)/2]);
+  // }
+  // return count.join("")
+// }
+// let result = y(nawstr);
+// console.log(result);
+// 
+
+
+// 5-masala--------------------------------------------------
+// let a = [true,"salom" ,123,"asc",122];
+// let count =[];
+// console.log(newA);
+// const y=(arr) =>{
+  // for (let i = 0; i < arr.length; i++) {
+    // if(arr[i] == Number){
+      // count.push(arr[i]);
+    // }
+  // }
+  // return count;
+// }
+// let result = y(a);
+// console.log(result);
+// 
+
+
+
+// 6-masala
+
+// let num = [1,2,3,43,62,5,2,73];
+// let count = 0;
+// 
+// const y=(arr) =>{
+  // for (let i = 0; i < arr.length; i++) {
+    // for (let j = 1; j < arr.length; j++) {
+      // if (num[i]>num[j]) {
+        // console.log(num[i]);
+      // }
+    // }
+  // }
+// 
+// }
+// y(num)
+// 
